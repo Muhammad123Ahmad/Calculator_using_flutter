36c915c066a4129460f4981f8ef69f298bb4c0f7
Muhammad123Ahmad <96855695+Muhammad123Ahmad@users.noreply.github.com>
2023-06-08 00:08:01 +0500

    Add files via upload

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+const Color grayColor = Colors.grey;
+
+const Color greenColor = Colors.green;
+
+const styleText = TextStyle(fontSize: 20, color: Colors.white);
